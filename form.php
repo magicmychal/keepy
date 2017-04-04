5ed550c7f12b2633764c61aff3125ddfce26a37a
Michal PAwlicki <michalpawlicki@outlook.com>
2017-04-04 19:30:08 +0200

    almost

--- form.php
+++ form.php
@@ -1,26 +1,46 @@
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = "";
-$name = $email = "";
+$nameErr = $emailErr = $contentErr = $titleErr = "";
+$name = $email = $content = $title = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
     $nameErr = "Name is required";
   } else {
     $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
   }
-    
-   if (empty($_POST["title"])) {
-    $titleErr = "Title is required";
+
+    if (empty($_POST["title"])) {
+    $nameErr = "Title is required";
   } else {
     $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
   }
+
   
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
   } else {
     $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
   }
+    
+  if (empty($_POST["content"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["content"]);
+  }
+
 
 }
 
--- index.php
+++ index.php
@@ -1,59 +1,70 @@
 <!doctype html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keepy by Zuzanna and Michal </title>
     <link rel="stylesheet" type="text/css" href="kit/semantic.min.css">
-    <link rel="stylesheet" type="text/css" href="main.css"/>
-    
+    <link rel="stylesheet" type="text/css" href="main.css" />
+
     <!-- jQuery load -->
-    <script
-      src="https://code.jquery.com/jquery-3.1.1.min.js"
-      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-      crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <script src="kit/semantic.js"></script>
 
 </head>
+
 <body>
+    <?php
+    //form.php - script for validate
+    include 'form.php';
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+?>
 
-
-<div id="header">
-        <span id="big">Keepy</span>
-        by Zuza &amp; Michał
-        <span id="to_the_right">
+        <div id="header">
+            <span id="big">Keepy</span> by Zuza &amp; Michał
+            <span id="to_the_right">
             <i class="big write square icon active"></i>
             <i class="big sticky note icon"></i>
         </span>
-    </div>
-<div class="ui container bodystyle">    
-    <div class="ui six column centered grid">
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-          <div class="ui input">
-           <input type="text" name="title" placeholder="Title">
-          </div>
-            <div class="ui form">
-          <div class="content">
-            <label>Content</label>
-            <textarea></textarea>
-          </div></div>
-            <div class="ui input">
-              <input type="text" placeholder="Name" name="name">
+        </div>
+        <div class="ui container bodystyle">
+            <div class="ui six column centered grid">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
+                    <div class="ui fluid input">
+                        <input type="text" name="title" placeholder="Title" value="<?php echo $title;?>">
+                        <span class="error"></span>
+                    </div>
+                    <textarea class="txt_style" name="content" placeholder="Type here" rows="5" cols="40"><?php echo $content;?></textarea>
+                    <br><br>
+                    <div class="ui input">
+                        <input type="text" placeholder="E-mail" name="email" value="<?php echo $email;?>">
+                    </div>
+                    <br><br>
+                    <div class="ui input">
+                        <input type="text" name="name" value="<?php echo $name;?>">
+                    </div>
+                    <input class="ui big yellow button" type="submit" name="submit" value="Post it!">
+                </form>
             </div>
-            <div class="ui input">
-              <input type="text" placeholder="mail" name="email">
-            </div>
-            <button class="ui big yellow button" data-vivaldi-spatnav-clickable="1" type="submit" value="Publish" >
-              Post it!
-            </button>
-        </form>
-    </div>
-    <div class="ui six column centered grid">
-        <?php
-            echo 'hey';
-        ?>
-    </div>
-</div>
+            <div class="ui negative message">
+              <i class="close icon" data-vivaldi-spatnav-clickable="1"></i>
+              <div class="header">
+               Check errors
+              </div>
+              <p>
+               <?php echo $nameErr;?><br>
+               <?php echo $emailErr;?><br>
+               <?php echo $titleErr;?><br>
+               <?php echo $contentErr;?>
+            </p></div>
+        </div>
 
 </body>
+
 </html>
